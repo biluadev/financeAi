bc1198c043509c811b302d39c09b8f5989c4d1fd
biluadev <biluadevcod@gmail.com>
2025-02-09 14:54:43 +0100

    Table rows done!

---
+++ app/transactions/_columns/index.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { Transaction, TransactionType } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import TransactionTypeBadge from "../_components/type-badge";
+import { Button } from "@/app/_components/ui/button";
+import { PencilIcon, TrashIcon } from "lucide-react";
+
+export const TransactionColumns: ColumnDef<Transaction>[] = [
+  {
+    accessorKey: "name",
+    header: "Nome",
+  },
+
+  {
+    accessorKey: "type",
+    header: "Tipo",
+    cell: ({ row: { original: transaction } }) => (
+      <TransactionTypeBadge transaction={transaction} />
+    ),
+  },
+
+  {
+    accessorKey: "category",
+    header: "Categoria",
+  },
+
+  {
+    accessorKey: "paymentMethod",
+    header: "Método de pagamento",
+  },
+
+  {
+    accessorKey: "date",
+    header: "Data",
+    cell: ({ row: { original: transaction } }) =>
+      new Date(transaction.date).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+      }),
+  },
+
+  {
+    accessorKey: "amount",
+    header: "Valor",
+    cell: ({ row: { original: transaction } }) =>
+      new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(Number(transaction.amount)),
+  },
+
+  {
+    accessorKey: "actions",
+    header: "",
+    cell: () => {
+      return (
+        <div className="space-x-1">
+          <Button variant="ghost" className="text-muted-foreground" size="icon">
+            <PencilIcon />
+          </Button>
+
+          <Button variant="ghost" className="text-muted-foreground" size="icon">
+            <TrashIcon />
+          </Button>
+        </div>
+      );
+    },
+  },
+];
---
+++ app/transactions/page.tsx
@@ -0,0 +1,29 @@
+import { ArrowDownUpIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { TransactionColumns } from "./_columns";
+
+const TransactionsPage = async () => {
+  // Acessar as transações do meu banco de dados
+
+  const transactions = await db.transaction.findMany({});
+
+  return (
+    <div className="space-y-6 p-6">
+      {/* TÍTULO E BOTÃO */}
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+
+        <Button>
+          Adicionar transação
+          <ArrowDownUpIcon />
+        </Button>
+      </div>
+
+      <DataTable columns={TransactionColumns} data={transactions} />
+    </div>
+  );
+};
+
+export default TransactionsPage;
