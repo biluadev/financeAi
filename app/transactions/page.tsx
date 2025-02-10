db9001c0c2ce0a8e3bc4fd48e55b549355b50e16
biluadev <biluadevcod@gmail.com>
2025-02-10 18:53:28 +0100

    Form in build

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,8 +1,7 @@
-import { ArrowDownUpIcon } from "lucide-react";
-import { Button } from "../_components/ui/button";
 import { db } from "../_lib/prisma";
 import { DataTable } from "../_components/ui/data-table";
 import { TransactionColumns } from "./_columns";
+import AddTransactionButton from "../_components/add-transaction-button";
 
 const TransactionsPage = async () => {
   // Acessar as transações do meu banco de dados
@@ -15,10 +14,7 @@ const TransactionsPage = async () => {
       <div className="flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">Transações</h1>
 
-        <Button>
-          Adicionar transação
-          <ArrowDownUpIcon />
-        </Button>
+        <AddTransactionButton />
       </div>
 
       <DataTable columns={TransactionColumns} data={transactions} />
